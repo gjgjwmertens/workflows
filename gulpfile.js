294555867c6de75a02c9a7dcb3fdf13d3ffa6803
gjgjwmertens <G.J.G.J.W.Mertens@uva.nl>
2016-04-11 08:37:05 +0200

    added gulp concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,25 @@
 var gulp = require('gulp'),
    gutil = require('gulp-util'),
-   coffee = require('gulp-coffee');
+   coffee = require('gulp-coffee'),
+   concat = require('gulp-concat');
 
-var coffeeSources = ['components/coffee/*.coffee']
+var coffeeSources = ['components/coffee/*.coffee'];
+var jsSources = [
+   'components/scripts/rclick.js',
+   'components/scripts/pixgrid.js',
+   'components/scripts/tagline.js',
+   'components/scripts/template.js'
+];
+
+var env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+   outputDir = 'builds/development/';
+   sassStyle = 'expanded';
+} else {
+   outputDir = 'builds/production/';
+   sassStyle = 'compressed';
+}
 
 gulp.task('coffee', function () {
    gulp
@@ -11,3 +28,13 @@ gulp.task('coffee', function () {
                .on('error', gutil.log))
       .pipe(gulp.dest('components/scripts'))
 });
+
+gulp.task('js', function () {
+
+   gulp.task('js', function () {
+      gulp.src(jsSources)
+         .pipe(concat('script.js'))
+         .pipe(gulp.dest(outputDir + 'js'))
+   });
+
+})
